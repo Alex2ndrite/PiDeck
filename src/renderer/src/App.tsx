e780a31a495711cb7aa54702395bef8116cff397
bfzz <473812916@qq.com>
2026-07-24 22:45:31 +0800

    chore: remove dead currentSessionSendStateAtom import

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -53,7 +53,6 @@ import {
   currentSessionIdAtom,
   currentSessionRuntimeAtom,
   currentSessionRuntimeUiAtom,
-  currentSessionSendStateAtom,
   projectInventoryAtom,
   removeSessionComposerStateAtom,
   removeSessionStateAtom,
